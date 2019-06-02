41eafab5d97d5fd7507537fa2118b193916522c4
Pedro Chambino <pchambino@gmail.com>
2019-06-02 14:15:54 +0100

    Field#orientation

--- lib/field.rb
+++ lib/field.rb
@@ -4,5 +4,17 @@ class Field
   end
 
   def orientation
+    head, neck = @data.dig(:you, :body)
+    return :up unless neck
+
+    vertical = neck[:y] - head[:y]
+    horizontal = neck[:x] - head[:x]
+
+    case
+    when vertical > 0 then :up
+    when vertical < 0 then :down
+    when horizontal > 0 then :left
+    when horizontal < 0 then :right
+    end
   end
 end
--- test/field_test.rb
+++ test/field_test.rb
@@ -2,12 +2,15 @@ require 'field'
 
 class FieldTest < Minitest::Test
   def test_orientation
-    field = Field.new(
-      you: {
-        body: {
-        }
-      }
-    )
-    assert_equal :up, field.orientation
+    assert_orientation [{ x: 3, y: 3 }], :up
+    assert_orientation [{ x: 3, y: 3 }, { x: 3, y: 4 }], :up
+    assert_orientation [{ x: 3, y: 3 }, { x: 3, y: 2 }], :down
+    assert_orientation [{ x: 3, y: 3 }, { x: 4, y: 3 }], :left
+    assert_orientation [{ x: 3, y: 3 }, { x: 2, y: 3 }], :right
+    assert_orientation [{ x: 3, y: 3 }, { x: 2, y: 3 }, { x: 1, y: 3 }], :right
+  end
+
+  def assert_orientation body, orientation
+    assert_equal orientation, Field.new(you: { body: body }).orientation
   end
 end
